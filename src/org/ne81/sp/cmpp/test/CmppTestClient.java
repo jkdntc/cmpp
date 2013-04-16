78600d6a383ba0117b61c70e8974d1eae70b2f9c
jk <j6832@hotmail.com>
2013-04-16 15:41:02 +0800

    forgot something

--- src/org/ne81/sp/cmpp/test/CmppTestClient.java
+++ src/org/ne81/sp/cmpp/test/CmppTestClient.java
@@ -11,27 +11,27 @@ import org.ne81.sp.cmpp.Constants;
 public class CmppTestClient {
 	public static void main(String args[]) throws IOException, ClassNotFoundException,
 			SQLException, InterruptedException {
-		CmppClient client = new CmppClient("cmpp.properties");// 117.79.235.165
+		CmppClient client = new CmppClient("cmpp.properties");
 
 		client.addListener(new CmppLogToDbListener("jdbc.properties"));
 
 		client.start();
 		for (int i = 0; i < 10000; i++) {
-			while (!client.ready());
-				
+			while (!client.ready())
+				;
+
 			CmppSubmit submit = new CmppSubmit(Constants.CMPP2_VERSION, "serviceId", "1311111111",
 					"C00014", "01", "0", "50", "1311111111", "第一次", "linkid");
 
 			client.send(submit);
 		}
 		client.stop();
-		
-		
-		
+
 		client.start();
 		for (int i = 0; i < 10000; i++) {
-			while (!client.ready());
-				
+			while (!client.ready())
+				;
+
 			CmppSubmit submit = new CmppSubmit(Constants.CMPP2_VERSION, "serviceId", "1311111111",
 					"C00014", "01", "0", "50", "1311111111", "二次发送", "linkid");
 

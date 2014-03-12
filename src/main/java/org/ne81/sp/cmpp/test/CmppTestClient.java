301a37f1c7012cd546fcf868fbc51cba2abfc7a7
jiangkun <13245616@qq.com>
2014-03-12 18:09:57 +0800

    合并

--- src/main/java/org/ne81/sp/cmpp/test/CmppTestClient.java
+++ src/main/java/org/ne81/sp/cmpp/test/CmppTestClient.java
@@ -14,14 +14,14 @@ public class CmppTestClient {
 			SQLException, InterruptedException {
 		final CmppClient client = new CmppClient("cmpp.properties");
 
-		client.addListener(new CmppLogToDbListener("cmpp.properties"));
+		//client.addListener(new CmppLogToDbListener("cmpp.properties"));
 
 		client.start();
 		for (int i = 0; i < 1; i++) {
 			while (!client.ready())
 				;
-			CmppSubmit submit = new CmppSubmit(Constants.CMPP2_VERSION, "serviceId", "1311111111",
-					"01", "0", "50", "1311111111", "中文안녕하세요", "linkid");
+			CmppSubmit submit = new CmppSubmit(Constants.CMPP2_VERSION, "MJS0019907", "13651398480",
+					"01", "0", "10690179", "13651398480", "中文안녕하세요", "linkid");
 			submit.setMsgId(submit.getSequenceId());
 			// submit.setMsgFmt((byte) 8);
 			CmppSubmit submits[] = CmppUtil
